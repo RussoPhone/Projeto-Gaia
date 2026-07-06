eea9faf86f20ca563b485c613066ca47606771c0
RussoPhone <100falecido@gmail.com>
2026-07-05 22:47:57 -0300

    minor documentation fixes

--- core/simulation.py
+++ core/simulation.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-class Simulation:
+class Simulation: #È aqui que fica os parametros da simulação.
     def __init__(self, world, sky, renderer, gtime, simulation_duration=120, frame_delay=0.05):
         self.world = world
         self.sky = sky
@@ -10,25 +10,27 @@ class Simulation:
         self.simulation_duration = simulation_duration
         self.frame_delay = frame_delay
 
-    def clear_screen(self):
+    def clear_screen(self): #Limpa a tela
         os.system("cls" if os.name == "nt" else "clear")
 
     def run(self):
         while self.gtime.mtk < self.simulation_duration:
             self.clear_screen()
-
+            #mostra estado atual do mundo
             self.render()
 
+            #avança em 1 mtk
             self.gtime.adv()
+            #espera um teco pra prox animação
             time.sleep(self.frame_delay)
-
+    #mostra as informações do tempo atual
     def render(self):
         print(self.gtime.debug_text)
         print()
-
+        #calcula a luz atual a partir do tempo
         light = self.sky.get_light(self.gtime)
-
+        #mostra o valor da luz
         print(f"Light: {light}")
         print()
-
+        #desenha o mundo + luz atual
         print(self.renderer.render_lit(self.world, light))
--- core/tile.py
+++ core/tile.py
@@ -2,7 +2,7 @@ class Tile: #aqui fica os tiles. As peças no tabuleiro.
     def __init__(self, tile_type, symbol): #terá seu nome tipo e seu simbolo representante.
         self.tile_type = tile_type
         self.symbol = symbol
-
+#as tiles. Tipo e simbolo.
 GRASS = Tile("grass", ".")
 STONE = Tile("stone", "^")
 WATER = Tile("water", "~")

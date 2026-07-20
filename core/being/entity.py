a8a364e31021c89b39bd0e24c3d35a69c9181a85
RussoPhone <100falecido@gmail.com>
2026-07-19 21:05:38 -0300

    fix typo

--- core/being/entity.py
+++ core/being/entity.py
@@ -5,10 +5,10 @@ class Entity:
         self.symbol = symbol
         self.x = x
         self.y = y
-        self.las_action = "criado"
+        self.last_action = "criado"
 
     def record_action(self, action):
-        self.las_action = action
+        self.last_action = action
 
     def debug_text(self):
         return f"name={self.name} | pos=({self.x}, {self.y}) | last_action={self.last_action}"

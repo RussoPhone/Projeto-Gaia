ced2e2f8227f1779373a578a4e8036775c52a931
RussoPhone <100falecido@gmail.com>
2026-07-19 22:13:44 -0300

    change main

--- core/main.py
+++ core/main.py
@@ -1,32 +1,14 @@
-from core.ambient.world import World
-from core.being.entity import Entity
-from core.being.organism import Organism
-from core.being.body import Body
-from core.ambient.sky import Sky
-from core.simulation.renderer import Renderer
-from core.simulation.gtime import Gtime
-from core.simulation.simulation import Simulation
-from core.ambient.tile import WATER, FOOD
+from core.simulation.config import ScenarioConfig
+from core.simulation.scenario import build_scenario
 
-world = World(20, 10)
-world.set_tile(15, 5, WATER)
-world.set_tile(18, 5, FOOD)
-organism = Organism("maria", "@", 10, 5)
-world.add_entity(organism)
-sky = Sky()
-
-renderer = Renderer()
-
-gtime = Gtime(mtksptk=24)
-
-simulation = Simulation(
-    world=world,
-    sky=sky,
-    renderer=renderer,
-    gtime=gtime,
+config = ScenarioConfig(
+    seed =42,
+    world_width=20,
+    world_height=10,
+    num_organism=10,
     simulation_duration=120,
-    frame_delay=0.08
+    frame_delay=0.08,
 )
+
+simulation = build_scenario(config)
 simulation.run()
-
-
